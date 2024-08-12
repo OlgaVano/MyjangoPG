ecff69ecda2acaa541adeaca8743c6528cf490ce
Olga <molyath7@gmail.com>
2024-08-12 09:58:28 +0300

    agian

--- Firstjangoo/my_first_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse('Hallo Olga - you are cool genies')
